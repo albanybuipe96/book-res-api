a0f5fc79e7763dddd475a962c5c844e9ba990496
mohammed.rahman <mohammed.rahman@hubtel.com>
2024-02-15 13:29:01 +0000

    api error messages

---
+++ src/constants/errors.constants.ts
@@ -0,0 +1,6 @@
+
+export class ErrorMessages {
+    static EMAIL_IN_USE: string = 'Email already in use'
+    static NO_USER_FOUND: string = 'No user found with given id'
+    static INVALID_CREDENTIAL: string = 'Invalid credentials'
+}
