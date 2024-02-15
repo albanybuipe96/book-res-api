8babcebc8374791952c6dcf6a051488906380da8
mohammed.rahman <mohammed.rahman@hubtel.com>
2024-02-15 13:30:33 +0000

    added auth routes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,34 +1,47 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common'
+import { UsersService } from './services/users.service'
+import { CreateUserDto } from './dto/create-user.dto'
+import { UpdateUserDto } from './dto/update-user.dto'
+import { AuthService } from './services/auth.service'
+import { SignInDto } from './dto/signin.dto'
+import { Serialize } from 'src/interceptors/serialize.interceptor'
+import { UserDto } from './dto/user.dto'
 
+@Serialize(UserDto)
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly authService: AuthService,
+  ) { }
 
-  @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
+  @Post('auth/signup')
+  create(@Body() { email, password }: CreateUserDto) {
+    return this.authService.signup(email, password)
+  }
+
+  @Post('auth/signin')
+  signin(@Body() signInDto: SignInDto) {
+    return this.authService.signin(signInDto)
   }
 
   @Get()
-  findAll() {
-    return this.usersService.findAll();
+  fetchUsers() {
+    return this.usersService.find(null)
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
+    return this.usersService.findOne(+id)
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
+    return this.usersService.update(+id, updateUserDto)
   }
 
-  @Delete(':id')
+  @Delete(':id/delete')
   remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
+    return this.usersService.remove(+id)
   }
 }
