fab0d961b6537611961a1727b505dcefd1315cd0
albanybuipe96 <mohammedarahman96@gmail.com>
2024-02-18 07:06:29 +0000

    modified signin dto password message

--- src/constants/dto-message.constants.ts
+++ src/constants/dto-message.constants.ts
@@ -1,6 +1,6 @@
 
 export class DtoMessage {
     static EMPTY_EMAIL: string = 'Email field can\'t be empty'
-    static EMPTY_PASSWORD: string = 'Password field can\'t be empty'
+    static EMPTY_PASSWORD: string = 'Password field can\'t be empty or weak'
     static WEAK_PASSWORD: string = 'Password is too weak'
 }
